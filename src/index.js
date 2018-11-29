bc218692a0a716fda202a80228ffe41a093c7d81
andreaweb <deiainluv@gmail.com>
2018-11-29 21:09:14 -0200

    getting all the API stuff

--- src/index.js
+++ src/index.js
@@ -2,39 +2,52 @@
 	
 let repos = {};
 let faves = {};
-let user = {};
-console.log(faves, repos, user);
+
 setArrs();
 
 function setArrs(){
-	faves = fetchArrs('/starred');
-	repos = fetchArrs('/repos');
-	user = fetchArrs('');
-	console.log(faves, repos, user);
+	fetchArrs('');
+	fetchArrs('/repos');
+	fetchArrs('/starred');
 }
 
 function fetchArrs(params){
 	fetch('https://api.github.com/users/andreaweb'+params)
 	.then(res => res.json())
-	.then(data => data)
+	.then(data => {
+			if(params === ''){
+				let reps = document.querySelector('#repos');
+				reps.innerHTML = data.public_repos;
+				let follows = document.querySelector('#following');
+				follows.innerHTML = data.following;
+				let isFollowed = document.querySelector('#followers');
+				isFollowed.innerHTML = data.followers;
+				let pic = document.querySelector('#picture');
+				pic.innerHTML = data.avatar_url;
+			}else if(params === '/starred'){
+				faves = data;
+			}else{
+				repos = data;
+			}
+		}
+	)
 	// .catch(error => error)
 }
 
 function checkButton(e){
-	if(e.target.id === '#get-repos'){
+	if(e.target.id === 'get-repos'){
 		generateHTML(repos)
-	}else if(e.target.id === '#get-stars'){
+	}else if(e.target.id === 'get-stars'){
 		generateHTML(faves)
 	}
 }
 
 function generateHTML(arr){
-	console.log(arr);
+	console.log("Arr in Generate html"+arr);
 	let html = "";
 	for(let i = 0; i < arr.length; i++){
 		html = html + `<tr><td>${arr[i].name}</td></tr>`
 	}
-	console.log(user.following);
 	let el = document.querySelector('#table');
 	el.innerHTML = html;
 	return html;
@@ -47,28 +60,32 @@ function scrollToAnchor(e){
 	e.preventDefault();
 }
 
+function getFollowing(){
+	while(!user.following){return 'A API do Github está instável ou outro erro ocorreu.'}
+	return user.following
+}
 
-document.addEventListener("click", scrollToAnchor);
+function getFollowers(){
+	while(!user.followers){return 'A API do Github está instável ou outro erro ocorreu.'}
+	return user.followers
+}
+
+let menu = document.getElementById('menu');
+menu.addEventListener("click", scrollToAnchor);
 
 const githubHTML = `
 <aside class="card-aside">
-	<img class="card-img" src=${
-		'https://avatars1.githubusercontent.com/u/13103893?s=460&v=4' 
-		|| user.avatar_url}/>
+	<img class="card-img" id="picture" src="https://avatars1.githubusercontent.com/u/13103893?s=460&v=4"/>
 	<a class="card-link" href="#">VISITAR PERFIL</a>
 </aside>
 <div class="card-buttons">
 	<ul class="card-github">
-		<li>REPOSITÓRIOS: </li>
-		<li>SEGUIDORES: 
-		${'A API do Github está instável ou outro erro ocorreu.' || user.followers}
-		</li>
-		<li>SEGUINDO: 
-		${'A API do Github está instável ou outro erro ocorreu.' || user.following}
-		</li>
+		<li>REPOSITÓRIOS: <span id="repos">API instável, sem internet ou outro erro!</span></li>
+		<li>SEGUIDORES: <span id="followers">API instável, sem internet ou outro erro!</span></li>
+		<li>SEGUINDO: <span id="following">API instável, sem internet ou outro erro!</span></li>
 	</ul>
-	<button class="button" id="#get-repos">VER REPOSITÓRIOS</button>
-	<button class="button" id="#get-stars">VER FAVORITOS</button>
+	<button class="button" id="get-repos">VER REPOSITÓRIOS</button>
+	<button class="button" id="get-stars">VER FAVORITOS</button>
 </div>
 <section class="card-toggle">
 	<h5>LISTA DOS REPOSITÓRIOS</h5>
